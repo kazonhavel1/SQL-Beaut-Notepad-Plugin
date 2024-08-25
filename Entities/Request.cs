ac5e8a2ebdf714ac2263dc74cc22978f4f879713
kazonhavel1 <joaopedroescola63@gmail.com>
2024-08-25 20:47:48 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Request.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net;
+using System.Collections.Generic;
+
+internal class Request
+{
+    private static readonly HttpClient httpClient = new HttpClient(); // Aqui começamos a instacia da classe HTTP
+
+    public static async Task<string> enviaRequisicao(string txt)
+    {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://sqlformat.org/api/v1/format"); //Link da RQ
+
+            var collection = new List<KeyValuePair<string, string>>();
+            collection.Add(new("sql", $"{txt}"));
+            collection.Add(new("reindent", "1"));
+            collection.Add(new("identifier_case", "upper")); 
+            collection.Add(new("keyword_case", "upper"));   //Criação do Objeto para envio
+
+            var content = new FormUrlEncodedContent(collection); //Decodificação para FormURL
+            request.Content = content;
+
+            HttpResponseMessage resposta = await httpClient.SendAsync(request); // envio da RQ
+
+            string respostaString = await resposta.Content.ReadAsStringAsync();
+            if (resposta.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+
+            return $"Erro {resposta.StatusCode.GetHashCode()} {resposta.StatusCode} ao processar a requisição: {respostaString}";
+
+            }
+            else { return respostaString; }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,152 @@
+﻿using Kbg.NppPluginNET.PluginInfrastructure;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Kbg.NppPluginNET
+{
+    class Main
+    {
+        internal const string PluginName = "IndentadorSQL"; // Nome do Plugin
+        static string iniFilePath = null;
+        static bool someSetting = false;
+        static frmMyDlg frmMyDlg = null;
+        static int idMyDlg = -1;
+
+        public static void OnNotification(ScNotification notification)
+        {
+            // This method is invoked whenever something is happening in notepad++
+            // use eg. as
+            // if (notification.Header.Code == (uint)NppMsg.NPPN_xxx)
+            // { ... }
+            // or
+            //
+            // if (notification.Header.Code == (uint)SciMsg.SCNxxx)
+            // { ... }
+        }
+
+
+
+        internal static void CommandMenuInit()
+        {
+            StringBuilder sbIniFilePath = new StringBuilder(Win32.MAX_PATH);
+            Win32.SendMessage(PluginBase.nppData._nppHandle, (uint)NppMsg.NPPM_GETPLUGINSCONFIGDIR, Win32.MAX_PATH, sbIniFilePath);
+            iniFilePath = sbIniFilePath.ToString();
+            if (!Directory.Exists(iniFilePath)) Directory.CreateDirectory(iniFilePath);
+            iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
+            someSetting = (Win32.GetPrivateProfileInt("SomeSection", "SomeKey", 0, iniFilePath) != 0);
+
+            PluginBase.SetCommand(0, "MyCommand", indentaSql, new ShortcutKey(false, false, false, Keys.F10));
+        }
+
+       
+        private static IntPtr ObterHandleJanela()  // Obter o handle (ID) da janela atual
+        {
+            IntPtr janela = PluginBase.nppData._nppHandle;
+
+
+            return janela.ToInt64() == 1 ? PluginBase.nppData._scintillaSecondHandle : PluginBase.nppData._scintillaMainHandle;
+            // Validar se a janela está dividida se sim ela traz a tela secundaria, senão traz a principal
+        }
+
+        
+
+        public static string BuscaTextoJanela() // Selecionar texto dentro da janela
+        {
+            IntPtr handle_janela = ObterHandleJanela();
+
+            // obter o tamanho do texto
+            int tamanho = (int)Win32.SendMessage(handle_janela, (uint)SciMsg.SCI_GETTEXTLENGTH, IntPtr.Zero, IntPtr.Zero);
+
+            byte[] textoBuffer = new byte[tamanho + 1];
+
+            GCHandle handle = GCHandle.Alloc(textoBuffer, GCHandleType.Pinned); // criando um manipulador de objetos
+            IntPtr textoPtr = handle.AddrOfPinnedObject(); // torna-lo fixo na memória (impede que o garbage colletor intefira no objeto e cause efeitos inesperados
+            Win32.SendMessage(handle_janela, (uint)SciMsg.SCI_GETTEXT, new IntPtr(tamanho + 1), textoPtr); // obtendo o texto da janela
+            handle.Free(); //liberar o GC
+
+            // converter de bytes para string usando UTF-8
+            string texto = Encoding.UTF8.GetString(textoBuffer, 0, tamanho);
+
+            return texto;
+        }
+
+        // Mensagem inicial no comando HelloNpp
+        internal static void HelloNpp()
+        {
+        }
+
+        internal static async void indentaSql()
+        {
+            string texto = BuscaTextoJanela();
+            var utf8 = Encoding.UTF8;
+            byte[] utfBytes = utf8.GetBytes(texto);
+            texto = utf8.GetString(utfBytes, 0, utfBytes.Length);
+
+            try
+            {
+                if (string.IsNullOrEmpty(texto)) // se a janela estiver vazia não enviar a RQ
+                { 
+                   var retorno = "Não foi possível efetuar o processo pois essa janela está vazia!";
+                   MessageBox.Show(retorno);
+                }
+                else
+                {   
+                   if(texto.ToUpper().Contains("SELECT") == true) //Enviar a Requisiçao somente se houver "Select" em meio a string da janela
+                    {
+                        
+                        var retorno = await Request.enviaRequisicao(texto); //envia a string para a API
+                        substituiTexto(retorno);
+
+                    }
+                    else
+                    {
+
+                        MessageBox.Show("Não foi possível efetuar o processo pois o texto desta janela não é um comando SQL.");
+
+                    }
+                }
+
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Erro {e.ToString()} ao processar requisição: {e.Message}");
+            }
+
+
+        }
+
+
+        static void substituiTexto(string consulta)
+        {
+
+            IntPtr janela_handle = ObterHandleJanela();
+            Win32.SendMessage(janela_handle, SciMsg.SCI_CLEARALL, IntPtr.Zero, IntPtr.Zero); //limpa o texto atual da janela
+
+            var dados = JsonConvert.DeserializeObject<Dictionary<string, object>>(consulta); //Converte o retorno para DIC para conseguirmos acessar somente o valor Result
+
+            string resultado = dados["result"].ToString();
+
+            byte[] textoBytes = Encoding.UTF8.GetBytes(resultado); // Converte o texto para UTF-8
+
+            GCHandle handleTexto = GCHandle.Alloc(textoBytes, GCHandleType.Pinned);
+            IntPtr textoPtr = handleTexto.AddrOfPinnedObject();
+
+           
+            Win32.SendMessage(janela_handle, (uint)SciMsg.SCI_ADDTEXT, new IntPtr(resultado.Length), textoPtr);  // Altera o texto na janela pro resultado
+
+
+            handleTexto.Free(); // Liberar o buffer de texto     
+            
+        }
+
+
+    }
+}
